5c160c483e3213c4029657287947eee0cb410d03
Christoph Finkensiep <chfin@freenet.de>
2014-11-10 17:12:17 +0100

    add class representation of style format

---
+++ harmstyle.py
@@ -0,0 +1,34 @@
+class HarmonicStyle(object):
+    """Representation of a Harmonic Style"""
+    
+    def __init__(self, name, description, progressions):
+        self.name = name
+        self.description = description
+        self.progressions = progressions
+
+class ChordProgression(object):
+    """A single Chord Progession"""
+    
+    def __init__(self, weight, chords):
+        self.weight = weight
+        self.chords = chords
+
+class Chord(object):
+    """A set of simultaneous notes"""
+    
+    def __init__(self, notes):
+        self._notes = set(notes)
+        self._normalize()
+        
+    def _normalize(self):
+        self._notes = {x % 12 for x in self._notes}
+    
+    def containsNote(self, note):
+        """Tests, wheather the Chord contains the Note"""
+        return note in self._notes
+
+    def getNotes(self):
+        self._normalize()
+        notes = list(self._notes)
+        notes.sort()
+        return notes
