f5b3f945b394a2246d362ba817496180576b710c
Wolfgang Gross <wolfigross@gmx.de>
2014-11-20 18:05:32 +0100

    Update style_visualizer
    Uses note (number) representation
    add method to convert number representation in literals
    Visualize class take now key as input (in what key should it be
    visualized)

--- style_visualizer.py
+++ style_visualizer.py
@@ -5,36 +5,54 @@ from harmstyle import HarmonicStyle, ChordProgression, Chord
 class Visualizer(object):
     """Simple visualization tool"""
 
-    @staticmethod
-    def visualize(st_dat):
+    def __init__(self, key='C'):
+        self.scale = ['C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab', 'A', 'A#/Bb', 'B']
+        self.key = self.scale.index(key)
+
+    def convert_to_literals(self, chord):
+        """Takes a cohrd and converts it into list of literals with the key [0-11] = [C-B]"""
+        literals = []
+        for n in chord.get_notes():
+            literals.append(self.scale[(n + self.key) % 12])
+        return literals
+
+
+    def visualize(self, st_dat):
         print "==========================="
         print "==========================="
         print "Name: \t\t\t" + st_dat.name
-        print "Description: \t"+ st_dat.description
+        print "Description: \t" + st_dat.description
         print ""
-
 
         #sort the list in place
         st_dat.progressions.sort(key=lambda x: x.weight, reverse=True)
 
         #print the list of progressions
         for i in st_dat.progressions:
+            print "w: " + "%.2f" % i.weight + ' \t',
             for chord in i.chords:
-                print str(chord.get_notes()) + '\t',
-            print '\t\t' + i.description + "\t w: " + str(i.weight)
+                out = str(self.convert_to_literals(chord))
+                print out,
+                for tab in range(len(out), 24):
+                    print '',
+
+            print i.description
 
         print "==========================="
 
         #create copy of the progression list to work with
         ls = st_dat.progressions[:]
-        while len(ls)>0:
-            start_chord = ls[0].description
-            print ls[0].description,
+        while len(ls) > 0:
+            start_progression = ls[0]
+            for prog_chord in start_progression.chords:
+                print self.convert_to_literals(prog_chord),
             #remove used progression from list
             ls.pop(0)
+            print '->',
             for j in ls:
-                if start_chord[-1] == j.description[0]:
-                    print "-> "+j.description[-1],
+                if start_progression.chords[-1].get_notes() == j.chords[0].get_notes():
+                    for print_chord in j.chords:
+                        print self.convert_to_literals(print_chord),
                     ls.remove(j)
                     break
             print ""
