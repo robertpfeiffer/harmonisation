c4e40d5ba66e7d094ba1efb1dc3054af3b833928
Wolfgang Gross <wolfigross@gmx.de>
2015-01-23 10:46:58 +0100

    Add MusicXML Visulatisation
    run script run_vis first define son style path and then out.xml path

---
+++ run_vis.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from harmstyleio import HarmonicStyleIO
+from style_visualizer import Visualizer
+import sys
+
+if __name__=="__main__":
+    style=HarmonicStyleIO.harmonic_style_from_JSON_file(sys.argv[1])
+    outpath = sys.argv[2]
+
+vis = Visualizer('C')
+
+#get chord list out of style
+chordlist = []
+for prog in style.progressions:
+    for c in prog.chords:
+        chordlist.append(c.get_notes())
+print chordlist
+#open file the xml in wrote to
+outfile = open(outpath, "w")
+xml = vis.write_musicxml(chordlist)
+outfile.write(xml)
+outfile.close()
